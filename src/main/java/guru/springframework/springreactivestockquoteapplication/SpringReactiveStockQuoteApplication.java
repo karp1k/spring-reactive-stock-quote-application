0bbfe68ee515d65f7dac80879e46098578057854
Tom <desktop-vm-ubuntu>
2020-06-08 13:54:16 +0300

    init project

---
+++ src/main/java/guru/springframework/springreactivestockquoteapplication/SpringReactiveStockQuoteApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springreactivestockquoteapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactiveStockQuoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactiveStockQuoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/springreactivestockquoteapplication/SpringReactiveStockQuoteApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springreactivestockquoteapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactiveStockQuoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
