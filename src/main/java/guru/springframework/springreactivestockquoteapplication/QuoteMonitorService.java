cb3501427370791817115d5844de250103a55ca4
Tom <desktop-vm-ubuntu>
2020-06-08 16:01:56 +0300

    add startup listener to persist data to db

---
+++ src/main/java/guru/springframework/springreactivestockquoteapplication/QuoteMonitorService.java
@@ -0,0 +1,36 @@
+package guru.springframework.springreactivestockquoteapplication;
+
+import guru.springframework.springreactivestockquoteapplication.client.StockQuoteClient;
+import guru.springframework.springreactivestockquoteapplication.domain.Quote;
+import guru.springframework.springreactivestockquoteapplication.repositories.QuoteRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author kas
+ */
+@Slf4j
+@Service
+public class QuoteMonitorService {
+
+    private final StockQuoteClient client;
+    private final QuoteRepository repository;
+
+    public QuoteMonitorService(StockQuoteClient client, QuoteRepository repository) {
+        this.client = client;
+        this.repository = repository;
+    }
+
+    @EventListener
+    public void listen(ContextRefreshedEvent event) {
+        client.getQuoteStream().log("quote-monitor")
+                .subscribe(quote -> {
+                    Mono<Quote> savedQuoteMono = repository.save(quote);
+                    savedQuoteMono.subscribe(savedQuote -> log.info("Saved quote with id: {}", savedQuote.getId()));
+                });
+    }
+}
